62bcefbfcdfb56cc435328a664a3d1e10660f3a0
EmmanuelRdezG2 <itsmeemmadev@gmail.com>
2022-03-25 00:40:05 -0600

    grid component

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,52 +1,25 @@
 import React, { useState, useEffect } from 'react';
-import { GifGridItems } from './GifGridItem';
+import { getGifs } from '../helpers/getGifs';
+import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
 
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    getGifs();
-  }, [])
-
-  const getGifs = async () => {
-    const key = 'DG0KWU3807B5';
-    const search = 'random';
-    const searchParam = 'naruto';
-
-    const url = `https://g.tenor.com/v1/${search}?q=${searchParam}&key=${key}`;
-    const resp = await fetch(url);
-    const { results } = await resp.json();
-
-    const gifs = (gif => {
-      const gifData = [];
-
-      for (let i = 0; i < 10; i++) {
-        const id = gif[i]?.id;
-        const title = gif[i]?.content_description;
-        const url = gif[i]['media'][0]['gif']['url'];
-
-        gifData.push({ id, title, url });
-      }
-
-      return gifData;
-    })
-
-    console.log(gifs(results));
-    setImages(gifs(results));
-  }
+    getGifs(category).then(imgs => setImages(imgs))
+  }, [category])
 
   return (
-    <div>
-      <h3>{category}</h3>
+    <section className='gallery'>
       {
-        images.map( img => {
-          <GifGridItems
-              key={img.id}
-              {...img}
+        images.map(imge => {
+          return <GifGridItem
+            key={imge.id}
+            {...imge}
           />
         })
       }
-    </div>
+    </section>
   );
 }
